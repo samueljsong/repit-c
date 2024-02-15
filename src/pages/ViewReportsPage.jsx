1cefbcacae96fe0015e2377920091880f48dc86b
Winston Nguyen <winston_nguyenn@hotmail.com>
2024-02-15 15:35:21 -0800

    Created tabs for view page

--- src/pages/ViewReportsPage.jsx
+++ src/pages/ViewReportsPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+// import 'react-tabs/style/react-tabs.css';
 
 export default function ViewReportsPage() {
-  return (
-    <div className='pt-20'>
-        ViewReports
-    </div>
-  )
+    const [selectedTab, setSelectedTab] = useState(0)
+    const tabOne = useRef(null)
+    const tabTwo = useRef(null)
+    useEffect(() => {
+        
+    }, []);
+
+
+    return (
+        <div className='pt-20'>
+            ViewReports
+            <Tabs>
+                <TabList className={''}>
+                    <Tab className={''} onClick={() => setSelectedTab(0)} ref={tabOne}>Title 1</Tab>
+                    <Tab className={''} onClick={() => setSelectedTab(1)} ref={tabTwo}>Title 2</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                    </TabPanel>
+                    <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+            </Tabs>
+        </div>
+    )
 }
