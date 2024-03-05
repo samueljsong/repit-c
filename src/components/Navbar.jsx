ffca12777cec5a0b8ef5b919e582ce0f0004aca0
Winston Nguyen <winston_nguyenn@hotmail.com>
2024-03-05 09:27:31 -0800

    Fixed navbar to use Link component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   function Dropdown() {
@@ -7,11 +8,11 @@ export default function Navbar() {
 
   return (
     <nav className="flex justify-between px-2 py-4 border shadow-md items-center fixed w-full bg-white">
-      <a href='/'>
+      <Link to='/' >
         <div className='text-bcit-blue font-extrabold font-inter-extrabold text-xl hover:cursor-pointer'>
           R E P I T
         </div>
-      </a>
+      </Link>
         <div className='flex items-center hover:cursor-pointer' onClick={Dropdown}>
           <div className='flex flex-col w-8 h-8 rounded-md bg-bcit-blue text-white justify-center items-center text-sm'>
               SS {/* Put user initials here*/}
