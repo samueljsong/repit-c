7461a7fe3f4e908bd410ea5fb6df44ff2f242d5c
inaguu <ianjguani@gmail.com>
2024-03-21 17:55:03 -0700

    made black box tests for creating a report

---
+++ cypress/e2e/regular_create_report_empty_location.cy.js
@@ -0,0 +1,15 @@
+describe('A user creates a report', () => { 
+    it('fails to create a report', () => {
+        const base_url = Cypress.env("CYPRESS_BASE_URL");
+        cy.loginRegular();
+        cy.visit(base_url + "/create")
+        cy.get('input[name="title"]').type('testTitle');
+        cy.get('textarea[name="description"]').type('testDescription');
+
+        cy.get('div[name="cloudinaryWidget"').should('exist'); // Check if the cloudinary widget exsits
+
+        cy.get('button[name="submit"]').click();
+        cy.wait(500)
+        cy.url().should('eq', base_url + '/create'); // a report has successfully been created.
+    });
+})
