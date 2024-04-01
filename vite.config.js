d6f341bc5a1da34f44c6e0277c4f105ac864c039
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-31 20:43:01 -0700

    fix: rename back

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import istanbul from 'vite-plugin-istanbul';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-      react(),
-      // Only instrument code for coverage in testing environments
-      process.env.NODE_ENV === 'development' && istanbul({
+  plugins: [
+    react(),
+    // Only instrument code for coverage in testing environments
+    process.env.NODE_ENV === 'development' &&
+      istanbul({
         include: 'src/**', // Instrument only files in the src/ directory
         exclude: ['node_modules', 'test/', '**/*.test.js', '**/*.spec.js'],
         extension: ['.js', '.jsx'], // Include JS and JSX files
-      })
-    ],
-  });
+      }),
+  ],
+});
+
